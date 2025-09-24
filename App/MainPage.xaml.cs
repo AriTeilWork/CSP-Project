20d768147d4134b487d3f4dc02399b74264c04c9
AriTeil <118675580+AriTeilWork@users.noreply.github.com>
2025-09-24 13:39:45 +0300

    refactor: right way

---
+++ App/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace CSP_Project;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnHelloClicked(object sender, EventArgs e)
+    {
+        var userName = NameEntry.Text?.Trim();
+
+        if (string.IsNullOrWhiteSpace(userName))
+            HelloLabel.Text = "Hello!";
+        else
+            HelloLabel.Text = $"Hello {userName}";
+    }
+}
