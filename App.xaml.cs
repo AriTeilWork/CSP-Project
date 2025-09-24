1562fd8ad5107abce063093e5363a11d5ff5fcfc
AriTeil <118675580+AriTeilWork@users.noreply.github.com>
2025-09-24 13:00:30 +0300

    Initial .NET MAUI Hello World project

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CSP_Project
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
